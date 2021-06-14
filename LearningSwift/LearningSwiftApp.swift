0aab389b8b8f9a0df76a93257bb75a211768799e
DHV10 <supervietfs@gmail.com>
2021-06-14 20:59:32 +0700

    Initial Commit

---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by DHV on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LearningSwiftView()
+        }
+    }
+}
---
+++ LearningSwift/Views/LearningSwiftView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningSwift
+//
+//  Created by DHV on 14/06/2021.
+//
+
+import SwiftUI
+
+struct LearningSwiftView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct LearningSwiftView_Previews: PreviewProvider {
+    static var previews: some View {
+        LearningSwiftView()
+    }
+}
